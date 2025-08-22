a384662d2d3e9f6e8a52ff2cf7f07e6d273fc732
F.Tibor <tibi.furesz@gmail.com>
2025-08-22 12:11:40 +0200

    Add new matcher

--- clang-tools-extra/clang-tidy/bugprone/RegexCheck.cpp
+++ clang-tools-extra/clang-tidy/bugprone/RegexCheck.cpp
@@ -14,33 +14,51 @@ using namespace clang::ast_matchers;
 namespace clang::tidy::bugprone {
 
 void RegexCheck::registerMatchers(MatchFinder *Finder) {
-  Finder->addMatcher(cxxConstructExpr().bind("x"), this);
+  Finder->addMatcher(
+      cxxConstructExpr(
+          hasDeclaration(
+              cxxConstructorDecl(ofClass(classTemplateSpecializationDecl(
+                  anyOf(hasName("std::basic_regex"),
+                        hasName("boost::basic_regex")))))),
+          hasAnyArgument(ignoringImplicit(anyOf(
+              stringLiteral().bind("stringLiteral"),
+              declRefExpr(
+                  hasType(qualType(
+                      isConstQualified(),
+                      hasUnqualifiedDesugaredType(recordType(hasDeclaration(
+                          cxxRecordDecl(hasName("::std::basic_string"))))))))
+                  .bind("stringVar"),
+              declRefExpr(hasType(pointerType(
+                              pointee(builtinType(), isConstQualified()))))
+                  .bind("charptr")))))
+          .bind("x"),
+      this);
 }
 
-bool isValidRegex(std::string&& s){
+bool isValidRegex(std::string &&s) {
   llvm::Regex regex(s);
   return regex.isValid();
 }
 
-const Expr* InitRoute(const Expr* init){
+const Expr *InitRoute(const Expr *init) {
   // init is never null
-  const StringLiteral* str =
-    llvm::dyn_cast_or_null<StringLiteral>(init->IgnoreImpCasts());
-  if(str){
+  const StringLiteral *str =
+      llvm::dyn_cast_or_null<StringLiteral>(init->IgnoreImpCasts());
+  if (str) {
     if (!isValidRegex(str->getString().str()))
       return str;
   }
-  const CXXConstructExpr* string_constr =
-    llvm::dyn_cast_or_null<CXXConstructExpr>(init->IgnoreImpCasts());
-  if(!string_constr || string_constr->getNumArgs() < 1)
+  const CXXConstructExpr *string_constr =
+      llvm::dyn_cast_or_null<CXXConstructExpr>(init->IgnoreImpCasts());
+  if (!string_constr || string_constr->getNumArgs() < 1)
     return nullptr;
-  const Expr* arg = string_constr->getArg(0);
-  if(!arg)
+  const Expr *arg = string_constr->getArg(0);
+  if (!arg)
     return nullptr;
-  if(!arg->IgnoreImpCasts())
+  if (!arg->IgnoreImpCasts())
     return nullptr;
   str = llvm::dyn_cast_or_null<StringLiteral>(arg->IgnoreImpCasts());
-  if(str){
+  if (str) {
     if (!isValidRegex(str->getString().str()))
       return str;
   }
@@ -49,53 +67,64 @@ const Expr* InitRoute(const Expr* init){
 
 void RegexCheck::check(const MatchFinder::MatchResult &Result) {
   const Expr *expr = Result.Nodes.getNodeAs<Expr>("x");
-  if(!expr)
+  if (expr)
+    diag(expr->getBeginLoc(), "Invalid!") << expr->getSourceRange();
+  const Expr *stringlit = Result.Nodes.getNodeAs<Expr>("stringLiteral");
+  if (stringlit)
+    diag(stringlit->getBeginLoc(), "StringLit!") << stringlit->getSourceRange();
+  const Expr *stringvar = Result.Nodes.getNodeAs<Expr>("stringVar");
+  if (stringvar)
+    diag(stringvar->getBeginLoc(), "StringVar!") << stringvar->getSourceRange();
+  const Expr *charptr = Result.Nodes.getNodeAs<Expr>("charptr");
+  if (charptr)
+    diag(charptr->getBeginLoc(), "Charptr!") << charptr->getSourceRange();
+  if (!expr)
     return;
   clang::QualType type = expr->getType();
-  if(type->getCanonicalTypeInternal().getAsString().find("std::basic_regex") ==
-    std::string::npos &&
-    type->getCanonicalTypeInternal().getAsString().find("boost::basic_regex")
-    == std::string::npos)
+  return;
+  if (type->getCanonicalTypeInternal().getAsString().find("std::basic_regex") ==
+          std::string::npos &&
+      type->getCanonicalTypeInternal().getAsString().find(
+          "boost::basic_regex") == std::string::npos)
     return;
-  const CXXConstructExpr* constr =
-    llvm::dyn_cast_or_null<CXXConstructExpr>(expr);
-  if(!constr)
+  const CXXConstructExpr *constr =
+      llvm::dyn_cast_or_null<CXXConstructExpr>(expr);
+  if (!constr)
     return;
   const Expr *arg = constr->getArg(0);
-  if(!arg)
+  if (!arg)
     return;
   // StringLiteral as constructor argument
-  const StringLiteral* str =
-    llvm::dyn_cast_or_null<StringLiteral>(arg->IgnoreImpCasts());
-  if(str){
+  const StringLiteral *str =
+      llvm::dyn_cast_or_null<StringLiteral>(arg->IgnoreImpCasts());
+  if (str) {
     if (!isValidRegex(str->getString().str()))
-      diag(str->getBeginLoc(), "Invalid regex!") << str->getSourceRange();
+      diag(str->getBeginLoc(), "Invalid!") << str->getSourceRange();
     else
-      diag(str->getBeginLoc(), "Valid regex!") << str->getSourceRange();
+      diag(str->getBeginLoc(), "Valid!") << str->getSourceRange();
   }
   // Variable as constructor arg
   const DeclRefExpr *var =
-    llvm::dyn_cast_or_null<DeclRefExpr>(arg->IgnoreImpCasts());
-  if(!var)
+      llvm::dyn_cast_or_null<DeclRefExpr>(arg->IgnoreImpCasts());
+  if (!var)
     return;
-  const ValueDecl* baseDecl = var->getDecl();
-  if(!baseDecl)
+  const ValueDecl *baseDecl = var->getDecl();
+  if (!baseDecl)
     return;
-  const VarDecl* varDecl = llvm::dyn_cast_or_null<VarDecl>(baseDecl);
-  if(!varDecl || !varDecl->isConstexpr())
+  const VarDecl *varDecl = llvm::dyn_cast_or_null<VarDecl>(baseDecl);
+  if (!varDecl || !varDecl->isConstexpr())
     return;
 
   // INIT PART
   // getCanonicalDecl solves decls like extern std::string s;
   // to their external definition (is this true tho?)
-  const Expr* init = varDecl->getCanonicalDecl()->getInit();
-  if(init){
-      const Expr* report = InitRoute(init);
-      if(report)
-        diag(report->getBeginLoc(), "Invalid regex!")
-          << report->getSourceRange();
-      else
-        diag(init->getBeginLoc(), "Valid regex!") << init->getSourceRange();
+  const Expr *init = varDecl->getCanonicalDecl()->getInit();
+  if (init) {
+    const Expr *report = InitRoute(init);
+    if (report)
+      diag(report->getBeginLoc(), "Invalid!") << report->getSourceRange();
+    else
+      diag(init->getBeginLoc(), "Valid!") << init->getSourceRange();
   }
 }
 
