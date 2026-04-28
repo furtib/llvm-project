64cc1f3c03860fef8032ae7ad15d7989887324e8
F.Tibor <tibi.furesz@gmail.com>
2026-04-28 22:00:01 +0200

    Extend test suite

--- clang-tools-extra/test/clang-tidy/checkers/bugprone/regex.cpp
+++ clang-tools-extra/test/clang-tidy/checkers/bugprone/regex.cpp
@@ -34,6 +34,8 @@ class basic_regex {
 public:
     basic_regex(const T *){};
     basic_regex(const std::string &s) {}
+    template <class ForwardIt>
+    basic_regex(ForwardIt first, ForwardIt last) {} 
     ~basic_regex(){}
 };
 typedef basic_regex<char> regex;
@@ -44,14 +46,16 @@ class RE2{
 public:
     RE2(const char* q){};
     RE2(const std::string &s){};
+    template <class ForwardIt>
+    RE2(ForwardIt first, ForwardIt last) {} 
     ~RE2(){};
 };
 } // namespace re2
 
 // Triggers the check:
 void foo(){
-    std::regex("(");
-    // CHECK-MESSAGES: :[[@LINE-1]]:16: warning: Invalid regex pattern! [bugprone-regex]
+    std::regex re0("(?<=a)");
+    // CHECK-MESSAGES: :[[@LINE-1]]:20: warning: Invalid regex pattern! [bugprone-regex]
     const char* stdrepat1 = "+";
     // CHECK-MESSAGES: :[[@LINE-1]]:29: warning: Invalid regex pattern! [bugprone-regex]
     std::regex re1(stdrepat1);
@@ -63,33 +67,48 @@ void foo(){
     std::regex re3(stdrepat3.begin(), stdrepat3.end());
 
 
-    boost::regex("+");
-    // CHECK-MESSAGES: :[[@LINE-1]]:18: warning: Invalid regex pattern! [bugprone-regex]
+    boost::regex re4("+");
+    // CHECK-MESSAGES: :[[@LINE-1]]:22: warning: Invalid regex pattern! [bugprone-regex]
     const char* boostrepat1 = "a**";
     // CHECK-MESSAGES: :[[@LINE-1]]:31: warning: Invalid regex pattern! [bugprone-regex]
-    boost::regex re4(boostrepat1);
+    boost::regex re5(boostrepat1);
     const std::string boostrepat2("(");
     // CHECK-MESSAGES: :[[@LINE-1]]:35: warning: Invalid regex pattern! [bugprone-regex]
-    boost::regex re5(boostrepat2);
-    re2::RE2("+");
-    // CHECK-MESSAGES: :[[@LINE-1]]:14: warning: Invalid regex pattern! [bugprone-regex]
+    boost::regex re6(boostrepat2);
+    std::string_view boostrepat3("(");
+    // CHECK-MESSAGES: :[[@LINE-1]]:34: warning: Invalid regex pattern! [bugprone-regex]
+    boost::regex re7(boostrepat3.begin(), boostrepat3.end());
+
+
+    re2::RE2 re8("(?<=a)");
+    // CHECK-MESSAGES: :[[@LINE-1]]:18: warning: Invalid regex pattern! [bugprone-regex]
     const char* re2repat1 = "a**";
     // CHECK-MESSAGES: :[[@LINE-1]]:29: warning: Invalid regex pattern! [bugprone-regex]
-    re2::RE2 re6(re2repat1);
+    re2::RE2 re9(re2repat1);
     const std::string re2repat2("(");
     // CHECK-MESSAGES: :[[@LINE-1]]:33: warning: Invalid regex pattern! [bugprone-regex]
-    re2::RE2 re7(re2repat2);
+    re2::RE2 re10(re2repat2);
 }
 
 // Valid patterns that do not trigger the check:
 void correct_patterns() {
-    std::regex correct_std("[0-9]+");
-    boost::regex correct_boost("^[a-zA-Z]+$");
+    std::regex correct_std("^[a-zA-Z]+$");
+    boost::regex correct_boost("(?<=a)");
     re2::RE2 correct_re2("^[a-zA-Z]+$");
 
-    std::regex correct_std2{"[A-Z]*"};
-    boost::regex correct_boost2{"(a|b|c)"};
-    re2::RE2 correct_re22{"(a|b|c)"};
+    const char* correct_char = "(a|b|c)";
+    std::regex correct_std2(correct_char);
+    boost::regex correct_boost2(correct_char);
+    re2::RE2 correct_re22(correct_char);
+
+    const std::string correct_str = "[0-9]+";
+    std::regex correct_std3(correct_str);
+    boost::regex correct_boost3(correct_str);
+    re2::RE2 correct_re23(correct_str);
+
+    std::string_view correct_str_view = "a+";
+    std::regex correct_std4(correct_str_view.begin(), correct_str_view.end());
+    boost::regex correct_boost4(correct_str_view.begin(), correct_str_view.end());
 
     //Out of scope false negatives:
     // mutable string
